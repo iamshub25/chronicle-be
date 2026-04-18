5c755f023c8749dd050211bd6b845162a2058431
Iamshub25 <shubhampra25@gmail.com>
2026-04-18 19:44:44 +0530

    added path

--- modules/auth/routes.js
+++ modules/auth/routes.js
@@ -6,21 +6,21 @@ const Responder = require("../../services/ResponderService");
 const router = express.Router();
 
 // SIGNUP (Temporary - used to create first admin)
-// router.post("/signup", async (req, res) => {
-//   try {
-//     const { username, password } = req.body;
+router.post("/signup", async (req, res) => {
+  try {
+    const { username, password } = req.body;
 
-//     const existingUser = await User.findOne({ username });
-//     if (existingUser) return Responder.respondWithError(req, res, "User already exists");
+    const existingUser = await User.findOne({ username });
+    if (existingUser) return Responder.respondWithError(req, res, "User already exists");
 
-//     const newUser = new User({ username, password });
-//     await newUser.save();
+    const newUser = new User({ username, password });
+    await newUser.save();
 
-//     return Responder.respondWithSuccess(req, res, { username: newUser.username }, "Account created successfully!");
-//   } catch (error) {
-//     return Responder.respondWithError(req, res, error);
-//   }
-// });
+    return Responder.respondWithSuccess(req, res, { username: newUser.username }, "Account created successfully!");
+  } catch (error) {
+    return Responder.respondWithError(req, res, error);
+  }
+});
 
 // LOGIN
 router.post("/login", async (req, res) => {
